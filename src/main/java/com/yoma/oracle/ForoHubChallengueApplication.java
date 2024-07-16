b986d6ab422257359d0d9864d8e5f2a78af7bb68
JonaCruz <jactj2@hotmail.com>
2024-07-16 14:02:35 -0700

    primmer commit proyecto en blanco

---
+++ src/main/java/com/yoma/oracle/ForoHubChallengueApplication.java
@@ -0,0 +1,13 @@
+package com.yoma.oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubChallengueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubChallengueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoma/oracle/ForoHubChallengueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoma.oracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubChallengueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
